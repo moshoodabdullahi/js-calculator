fbe6155ef59784ff5572308f3711537f41c927c4
Moshood Abdullahi Ayomide <moshoodabdullai442@gmail.com>
2023-11-07 02:52:30 +0100

    feat: add theme implementation project

--- js/index.js
+++ js/index.js
@@ -1,6 +1,24 @@
-const startApp = () => {
-  const body = document.querySelector('body');
-  body.value = 'Hello, World';
-};
+const theme1Element = document.querySelector('#theme-1-attr');
+const theme2Element = document.querySelector('#theme-2-attr');
+const theme3Element = document.querySelector('#theme-3-attr');
 
-startApp();
+theme1Element.addEventListener('change', () => {
+  if (theme1Element.checked === true) {
+    document.body.classList.remove('theme2');
+    document.body.classList.remove('theme3');
+  }
+});
+
+theme2Element.addEventListener('change', () => {
+  if (theme2Element.checked === true) {
+    document.body.classList.remove('theme3');
+    document.body.classList.add('theme2');
+  }
+});
+
+theme3Element.addEventListener('change', () => {
+  if (theme3Element.checked === true) {
+    document.body.classList.remove('theme2');
+    document.body.classList.add('theme3');
+  }
+});
