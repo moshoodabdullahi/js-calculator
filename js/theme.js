2810f3a56fccd27aaf54001e86e3b43ead05eb46
Moshood Abdullahi Ayomide <moshoodabdullai442@gmail.com>
2023-11-07 13:50:03 +0100

    feat: made radio button to switch between theme

--- js/theme.js
+++ js/theme.js
@@ -1,24 +1,4 @@
-const theme1Element = document.querySelector('#theme-1-attr');
-const theme2Element = document.querySelector('#theme-2-attr');
-const theme3Element = document.querySelector('#theme-3-attr');
-
-theme1Element.addEventListener('change', () => {
-  if (theme1Element.checked) {
-    document.body.classList.remove('theme2');
-    document.body.classList.remove('theme3');
-  }
-});
-
-theme2Element.addEventListener('change', () => {
-  if (theme2Element.checked) {
-    document.body.classList.remove('theme3');
-    document.body.classList.add('theme2');
-  }
-});
-
-theme3Element.addEventListener('change', () => {
-  if (theme3Element.checked) {
-    document.body.classList.remove('theme2');
-    document.body.classList.add('theme3');
-  }
+document.themeForm.addEventListener('change', (e) => {
+  const changeThemeColor = e.target.value;
+  document.body.className = changeThemeColor;
 });
