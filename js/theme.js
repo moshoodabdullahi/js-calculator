a245f08e537d434a133f2f6f1fec6e740b7515e2
Moshood Abdullahi Ayomide <moshoodabdullai442@gmail.com>
2023-11-07 09:13:25 +0100

    feat: separate input and label element for better understanding

--- js/theme.js
+++ js/theme.js
@@ -3,21 +3,21 @@ const theme2Element = document.querySelector('#theme-2-attr');
 const theme3Element = document.querySelector('#theme-3-attr');
 
 theme1Element.addEventListener('change', () => {
-  if (theme1Element.checked === true) {
+  if (theme1Element.checked) {
     document.body.classList.remove('theme2');
     document.body.classList.remove('theme3');
   }
 });
 
 theme2Element.addEventListener('change', () => {
-  if (theme2Element.checked === true) {
+  if (theme2Element.checked) {
     document.body.classList.remove('theme3');
     document.body.classList.add('theme2');
   }
 });
 
 theme3Element.addEventListener('change', () => {
-  if (theme3Element.checked === true) {
+  if (theme3Element.checked) {
     document.body.classList.remove('theme2');
     document.body.classList.add('theme3');
   }
