57ed7cc0bbbaa85397aebfd62a57626226327600
Moshood Abdullahi Ayomide <moshoodabdullai442@gmail.com>
2023-11-08 14:52:58 +0100

    feat: write the test of each operator separately

--- js/operate.test.js
+++ js/operate.test.js
@@ -2,7 +2,16 @@ const operate = require('./operate');
 
 test('adds 4 + 2 to equal 6', () => {
   expect(operate(4, 2, '+')).toBe(6);
+});
+
+test('adds 4 - 2 to equal 2', () => {
   expect(operate(4, 2, '-')).toBe(2);
+});
+
+test('adds 4 * 2 to equal 8', () => {
   expect(operate(4, 2, '*')).toBe(8);
+});
+
+test('adds 4 / 2 to equal 2', () => {
   expect(operate(4, 2, '/')).toBe(2);
 });
