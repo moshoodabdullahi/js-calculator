f18731ec821ba0e5940e6f047dd22f7cf3afaee2
Moshood Abdullahi Ayomide <moshoodabdullai442@gmail.com>
2023-12-29 07:27:31 +0100

    feat: add operator test case to calculate.test file

--- js/calculate.test.js
+++ js/calculate.test.js
@@ -42,3 +42,41 @@ describe("when 'input' is a number", () => {
     expect(calculation).toHaveProperty('operator', '+');
   });
 });
+
+describe("when 'input' is an operator", () => {
+  it('does not update any value when all value are null', () => {
+    data = { operandOne: null, operandTwo: null, displayValue: null, operator: null };
+    const calculation = calculate('+', data);
+    expect(calculation).toHaveProperty('operandOne', null);
+    expect(calculation).toHaveProperty('operandTwo', null);
+    expect(calculation).toHaveProperty('displayValue', null);
+    expect(calculation).toHaveProperty('operator', null);
+  });
+
+  it('updates operator when operator is null and, operandOne is not null', () => {
+    data = { operandOne: '2', operandTwo: null, displayValue: '2', operator: null };
+    const calculation = calculate('+', data);
+    expect(calculation).toHaveProperty('operandOne', '2');
+    expect(calculation).toHaveProperty('operandTwo', null);
+    expect(calculation).toHaveProperty('displayValue', '2');
+    expect(calculation).toHaveProperty('operator', '+');
+  });
+
+  it('updates operator when operandTwo is null, operandOne and operator are not null', () => {
+    data = { operandOne: '2', operandTwo: null, displayValue: '2', operator: '+' };
+    const calculation = calculate('-', data);
+    expect(calculation).toHaveProperty('operandOne', '2');
+    expect(calculation).toHaveProperty('operandTwo', null);
+    expect(calculation).toHaveProperty('displayValue', '2');
+    expect(calculation).toHaveProperty('operator', '-');
+  });
+
+  it('updates all values when all values are not null', () => {
+    data = { operandOne: '2', operandTwo: '3', displayValue: '5', operator: '+' };
+    const calculation = calculate('-', data);
+    expect(calculation).toHaveProperty('operandOne', '2');
+    expect(calculation).toHaveProperty('operandTwo', null);
+    expect(calculation).toHaveProperty('displayValue', '5'); 
+    expect(calculation).toHaveProperty('operator', '-');
+  });
+});
