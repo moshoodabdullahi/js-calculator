74561c9c64c316a2b30042e537d5e7afd54c6d94
Moshood Abdullahi Ayomide <moshoodabdullai442@gmail.com>
2024-01-13 09:15:36 +0100

    feat: made use of includes to lessen repetition of code

--- js/calculate.js
+++ js/calculate.js
@@ -2,18 +2,7 @@ const operate = require('./operate');
 
 function calculate(input, calcObject) {
   let { operandOne = null, operandTwo = null, displayValue = null, operator = null } = calcObject;
-  if (
-    input === '0' ||
-    input === '1' ||
-    input === '2' ||
-    input === '3' ||
-    input === '4' ||
-    input === '5' ||
-    input === '6' ||
-    input === '7' ||
-    input === '8' ||
-    input === '9'
-  ) {
+  if (['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'].includes(input)) {
     if (operandOne === null) {
       operandOne = input;
       displayValue = operandOne;
